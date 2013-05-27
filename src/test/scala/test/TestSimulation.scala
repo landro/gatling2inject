b940c32c1d336a0b4be9b4f8816b66baa07f559c
Stefan Magnus Landrø <stefan.landro@gmail.com>
2013-05-27 22:15:34 +0200

    Updated code to work against master

--- src/test/scala/test/TestSimulation.scala
+++ src/test/scala/test/TestSimulation.scala
@@ -14,46 +14,33 @@ import assertions._
 
 class TestSimulation extends Simulation {
 
-  System.setProperty("http.proxyHost", "localhost")
-  System.setProperty("http.proxyPort", "8888")
-
-
-
   val httpProtocol = http
-    .baseURL("http://localhost:8080/")
+    .baseURL("http://localhost:8000/")
     .disableWarmUp
 
   val userids = csv("userids.txt").random
 
-  // We actually use a custom saml library, but I've modified this to just create a createSamlAssertion instead
+  // We actually use a custom saml library, but I've modified this to just create a dummy instead
   def createSamlAssertion(s: String) : String = {
-    new java.lang.String(MessageDigest.getInstance("MD5").digest(s.getBytes), "UTF8")
+    "prefix_" + s + "_suffix"
   }
 
 
   val injectSamlAssertion = (session: Session) =>  {
-    session.getV[String]("userid").map(userid =>
+    session("userid").validate[String].map(userid =>
       session.setAll(Map("userid" -> userid, "saml" -> createSamlAssertion(userid))))
-  }
-
-  val testFunc = (session: Session) => {
-    println(session.get("saml"))
-    println(session.get("userid"))
-    session.getV[String]("saml").map(saml =>
-      session.setAll(("key" ,"value"))
-    )
   }
 
   val scn = scenario("post userids")
     .feed(userids)
     .exec(injectSamlAssertion)
-    .exec(testFunc)
     .exec(
       http("Call service")
         .post("service")
         .headers(Map("Content-Type" -> "multipart/related; type=\"application/xop+xml\";"))
         .elFileBodyPart("nameofbodypart", "test.ssp", "application/xml" )
         .rawFileBodyPart("part2", "test.pdf", "application/pdf", "somecontentid" )
+
       )
 
   setUp(scn.inject(ramp(1 users) over (1 second))).protocols(httpProtocol)
